b4fd9fafed864591ede3842edc4cd1749696a1bb
Elvis <eatq.97@gmail.com>
2022-07-15 09:20:17 -0500

    first commit

---
+++ ClienteEcoNoBloquante/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace ClienteEcoNoBloqueante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inicialización de la clase tipo String
+            // llamado servidor como localhost
+            String servidor = "localhost";
+            //Inicialización de buffer de transmisión
+            //e inicialización de buffer de recepción
+            byte[] buferTx = new byte[512];
+            byte[] buferRx = new byte[512];
+            //inicialización de puerto con el que se trabajará
+            int puerto = 8080;
+            //Inicialización de socket cliente en null
+            Socket socketCliente = null;
+            try
+            {
+                //En caso de que existe alguna excepción
+                //Creo un objeto del tipo socket que paso como parametros
+                //de entrada la direccion, tipo de socket y protocolo
+                socketCliente = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+                ProtocolType.Tcp);
+                //Realizo la conexión del socket mediante una direccion 
+                //y un puerto
+                socketCliente.Connect(new IPEndPoint(Dns.GetHostEntry(servidor).AddressList[1],
+                puerto));
+            }
+            catch (Exception e)
+            {
+                //Mensaje de error
+                Console.WriteLine(e.Message);
+                Environment.Exit(-1);
+            }
+            //Inicialización de contadores de tipo int
+            //Que representan los bytes enviados y recibidos
+            int totalBytesEnviados = 0;
+            int totalBytesRecibidos = 0;
+            //Almacenamiento en el buffer transmisión 
+            //la cadena codificada en ascci
+
+            buferTx = Encoding.ASCII.GetBytes("preparando: 5,3,2,1,0, GG well played!");
+
+            //buferTx = Encoding.ASCII.GetBytes("ESTOY ENVIANDO TODO ESTOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!");
+
+            //Socket del cliente no se le permite bloquear
+            socketCliente.Blocking = false;
+            //sentencia de control while
+            //Controla que los bytes rrecibidos sean menores que el tamaño del buffer
+            while (totalBytesRecibidos < buferTx.Length)
+            {
+                //Sentencia condicional en caso que los bytes enviados sean
+                //menores que el tamaño del bufer
+                if (totalBytesEnviados < buferTx.Length)
+                {
+                    //Se procede a realizar control de errores
+                    try
+                    {
+                        //Si existe incoherencia en el numero de
+                        //bytes enviados
+                        totalBytesEnviados += socketCliente.Send(buferTx, totalBytesEnviados,
+                        buferTx.Length - totalBytesEnviados, SocketFlags.None);
+                        Console.WriteLine("Se han enviado un total de {0} bytes al servidor...",
+                        totalBytesEnviados);
+                    }
+                    catch (SocketException se)
+                    {
+                        //Aviso que se ha establecido una excepcion
+                        if (se.ErrorCode == 10035)
+                        {
+                            //WSAEWOULDBLOCK: Recurso temporalmente no disponible
+                            Console.WriteLine("Temporalmente no es posible enviar, se reintentará despues...");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine(se.ErrorCode + ": " + se.Message);
+                            socketCliente.Close();
+                            Environment.Exit(se.ErrorCode);
+                        }
+                    }
+                }
+                try
+                {
+                    //En caso que se esté recibiendo el total de 
+                    //bytes desde el servidor se procede a corregir
+                    //el error
+                    int bytesRecibidos = 0;
+                    if ((bytesRecibidos = socketCliente.Receive(buferRx, totalBytesRecibidos,
+                    buferRx.Length - totalBytesRecibidos, SocketFlags.None)) == 0)
+                    {
+                        Console.WriteLine("La conexion se cerro prematuramente...");
+                        break;
+                    }
+                    totalBytesRecibidos += bytesRecibidos;
+                }
+                catch (SocketException se)
+                {
+
+                    //Excepción en caso de
+                    //bytes recibidos
+                    if (se.ErrorCode == 10035)
+                        continue;
+                    else
+                    {
+                        Console.WriteLine(se.ErrorCode + ": " + se.Message);
+                        break;
+                    }
+                }
+                //Invocación del método realizar procesamiento
+                RealizarProcesamiento();
+            }
+            //Codificación en asccii de los bytes recibidos
+            //que se encuentran en el buffer de recepción
+            Console.WriteLine("Se han recibido {0} bytes desde el servidor: {1}", totalBytesRecibidos,
+            Encoding.ASCII.GetString(buferRx, 0, totalBytesRecibidos));
+            //cerramos el socket
+            socketCliente.Close();
+            //Establecimiento de tiempo de comunicación entre el cliente y servidor
+        }
+        static void RealizarProcesamiento()
+        {
+            Console.WriteLine(".");
+            Thread.Sleep(2000);
+        }
+    }
+}
---
+++ ServidorEcoConTimeOut/Program.cs
@@ -0,0 +1,156 @@
+﻿/*
+// Practica 04
+// Integrantes:
+//      Elvis Toscano
+//      Patricio Vaicilla
+// Fecha de entrega: 17/06/2022
+------------------------------------------
+ Resultados Ejercicio 1: 
+"   Uso de Timeouts y llamadas no bloqueantes"
+   - Existe un tiempo límite de espera en el servidor cuando hay de por medio información que tiene que llegar
+   - cuando este tiempo de espera llega a su límite entonces el envío de información no podrá realizarse
+   -Además, existe la posibilidad que si se realiza modificaciones en los buffers, específicamente en las longitueds
+   - ocasionará que los datos no se envien de manera correcta.
+--------------------------------
+ Resultados Ejercicio 2: "Framing y codificacion binaria textual"
+ 
+   
+   -Al realizar el cambio para que se envíe y se reciban los dos elementos no hay variación en la pérdida de paquetes
+   -es decir no hay problema que los dos se envíen o reciban
+   -Es importante definir que la forma de representar ya sea binaria o textual es la misma en bits. adicional para la realizacion de la 
+   -codificacion y decodificacion es crucial ubicar todas las variables necesarias para su realización
+
+-----------------------------------------------------------------------------------------------------------------------------------
+ Conclusiones
+  * Debido a que en el uso de timeouts y llamadas no bloqueantes existe un limite de espera, cuando la comunicación falla por algún motivo
+  * existe un tiempo límite de espera lo que permite al sistema ser más efectivo y no desperdiciar recursos.
+---------------------------------------------------------------------------------------------------------------------------------------------------
+ Recomendaciones
+  * Tener en cuenta que para la realización de la decodificación y codificación es crucial ubicar todas las variables que se requiera
+  *
+
+*/
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+namespace ServidorEcoConTimeout
+{
+    class Program
+    {
+        //inicialización de constantes tipo privadas
+        //para darle encapsulación y sean de caracter
+        //global
+        private const int TAM_BUFFER = 32;
+        private const int TAM_COLA = 5;
+        private const int LIMITE_ESPERA = 10000;
+        static void Main(string[] args)
+        {
+            //Inicialización de puerto
+            int puerto = 8080;
+            //Inicialización de socket llamado servidor en null
+            Socket servidor = null;
+            //Tratamiento de errores mediante try
+            try
+            {
+                //En caso de que exista una excepción
+                //se inicializará este código.
+                //Se crea un objeto del tipo socket en el cual se pasa
+                //como parámetros direccion, tipo de socket y tipo de protocolo
+                servidor = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+                ProtocolType.Tcp);
+                //Se tiene como parámetros de entrada en el terminal de un 
+                //socket una dirección y un puerto
+                servidor.Bind(new IPEndPoint(IPAddress.Any, puerto));
+                //Socket se encuentra escuchando
+                servidor.Listen(TAM_COLA);
+            }
+            catch (SocketException se)
+            {
+                //para verificar que se da una excepción se imprime
+                //un mensaje de error
+                Console.WriteLine(se.ErrorCode + ": " + se.Message);
+                Environment.Exit(se.ErrorCode);
+            }
+            //Inicialización de variables usadas en RX
+            byte[] buferRx = new byte[TAM_BUFFER];
+            int cantBytesRecibidos;
+            int totalBytesEnviados = 0;
+            //Sentencia de control de tipo iteración 
+            //sin salida
+            for (; ; )
+            {
+                Socket cliente = null;
+                try
+                {
+
+                    //Captura de excepción
+                    //Creación de socket dirigido al servidor
+                    cliente = servidor.Accept();
+                    DateTime tiempoInicio = DateTime.Now;
+                    //Método usado por el socket que define un 
+                    //limite de espera
+                    cliente.SetSocketOption(SocketOptionLevel.Socket,
+                    SocketOptionName.ReceiveTimeout, LIMITE_ESPERA);
+                    //Visualización extremo remoto del cliente
+                    Console.Write("Gestionando al cliente: " + cliente.RemoteEndPoint
+                    + " - ");
+                    totalBytesEnviados = 0;
+                    //sentencia de control iterativa sin asignar un
+                    //límite de iteraciones
+                    //relaciona cantidad de bytes recibidos frente a la cantidad de bytes
+                    //que debería recibir el cliente 
+                    while ((cantBytesRecibidos = cliente.Receive(buferRx, 0,
+                    buferRx.Length, SocketFlags.None)) > 0)
+                    {
+                        //Preparación del servidor para la transferencia de datos
+                        cliente.Send(buferRx, 0, cantBytesRecibidos,
+                        SocketFlags.None);
+                        totalBytesEnviados += cantBytesRecibidos;
+                        TimeSpan tiempoTranscurrido = DateTime.Now - tiempoInicio;
+                       //Sentencia de control condicional del tiempo límitede espera
+                        if (LIMITE_ESPERA - tiempoTranscurrido.TotalMilliseconds <
+                        0)
+                        {  
+                            //Tiempo finaliza con el cierre de la conexion
+                            //con el cliente
+                            Console.WriteLine("Terminando la conexión con el cliente debido al temporizador.Se han superado los "
+                                + LIMITE_ESPERA + "ms; se han enviado " + totalBytesEnviados
+                            + " bytes");
+                            cliente.Close();
+                            throw new SocketException(10060);
+                        }
+                        //Para un tipo de valor entero se realiza el establecimiento
+                        //de un socket
+                        cliente.SetSocketOption(SocketOptionLevel.Socket,
+                        SocketOptionName.ReceiveTimeout, (int)(LIMITE_ESPERA -
+                        tiempoTranscurrido.TotalMilliseconds));
+                    }
+                    //Impresión del número de bytes (8bits) a enviarse
+                    Console.WriteLine("Se han enviado {0} bytes.",
+                    totalBytesEnviados);
+                    cliente.Close();
+                }
+                catch (SocketException se)
+                {
+                    //Sentencia de control condicional que permite controlar
+                    //el total de información enviada
+                    //En caso de suceder esto, se visualizará un sms de error
+                    //Finalmente se acaba la conexión
+                    if (se.ErrorCode == 10060)
+                    {
+                        //Terminación de la conexión
+                        Console.WriteLine("Terminado la conexion debido al temporizador.Han transcurrido " 
+                            + LIMITE_ESPERA + "ms; se han transmitido " + totalBytesEnviados + " bytes");
+                    }
+                    else
+                    {
+                        //Mensaje de error
+                        Console.WriteLine(se.ErrorCode + ": " + se.Message);
+                    }
+                    cliente.Close();
+                }
+            }
+        }
+    }
+}
